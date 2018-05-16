d5d6f5963e4002f5d402a0ff4f2aa07f1c22600f
Stocco <renato.stoco@pismo.com.br>
2018-05-16 10:18:22 -0300

    correct import

--- src/main/kotlin/uy/kohesive/vertx/sqs/impl/SqsClientImpl.kt
+++ src/main/kotlin/uy/kohesive/vertx/sqs/impl/SqsClientImpl.kt
@@ -6,7 +6,7 @@ import com.amazonaws.AmazonWebServiceResult
 import com.amazonaws.ResponseMetadata
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.handlers.AsyncHandler
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
